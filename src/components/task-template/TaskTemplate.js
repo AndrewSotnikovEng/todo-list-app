29187b45e3e02050b5cfa7e4a1dd5e86b1a5570a
Andrii Sotnikov <andrew.sotnikov@zoho.com>
2023-07-06 12:32:32 +0300

    Modified template win appearance.

--- src/components/task-template/TaskTemplate.js
+++ src/components/task-template/TaskTemplate.js
@@ -1,11 +1,32 @@
-import React from "react";
-import { Input, Select, Button, Flex } from '@chakra-ui/react'
-import { AiFillPlusCircle } from 'react-icons/ai';
+import React, { useState } from "react";
+import { Input, Select, Button, Flex, Grid, GridItem } from '@chakra-ui/react'
+import { AiFillPlusCircle, AiOutlineCheck } from 'react-icons/ai';
+import styles from "./TaskTemplate.css";
 
-const TaskTemplate = props => {
-  if(!props.show) {
+function TaskTemplate(props) {
+
+    const [ currentState, setCurrentState ] = useState("selecting")
+    const [ additionModeVisibility, setAdditionModeVisibility ] = useState("none")
+    const [ selectingModeVisibility, setSelectingModeVisibility ] = useState("inline")
+    // function showForState() {
+
+    //         console.log(currentState)
+    //         if (currentState == "selecting") {
+             
+    //         } else if (currentState == "adding") {
+    //           <Input
+    //             size="sm"
+    //             value={props.taskHeader}
+    //             borderWidth={1}
+    //             borderColor="black"
+    //           />;
+    //         }
+    //       }
+    // }
+
+    if(!props.show) {
     return null
-  }
+    }
 
   return (
     <div className="modal">
@@ -14,35 +35,75 @@ const TaskTemplate = props => {
           {/* <h4 className="modal-title"> {props.taskHeader} </h4> */}
         </div>
         <div className="modal-body">
-          <Input
-            size="sm"
-            value={props.taskHeader}
-            borderWidth={1}
-            borderColor="black"
-          />
-
-          <Flex>
-            <Select
-              size="sm"
-              placeholder="Select template"
-              borderWidth={1}
-              borderColor="black"
-              mt={2}
-            >
-              <option value="task1">Task template 1</option>
-              <option value="task2">Task template 2</option>
-            </Select>
-            <Button
-              leftIcon={<AiFillPlusCircle />}
-              size="sm"
-              ml={3}
-              mt={2}
-              pl={3}
-              pr={1}
-              borderWidth={1}
-              borderColor={"black"}
-            />
-          </Flex>
+          <Grid
+            templateRows="repeat(2, 1fr)"
+            templateColumns="repeat(2, 1fr)"
+            maxWidth={"500px"}
+          >
+            <GridItem rowSpan={1} colSpan={2}>
+              <Input
+                size="sm"
+                value={props.taskHeader}
+                borderWidth={1}
+                borderColor="black"
+                maxWidth={"450px"}
+              />
+            </GridItem>
+            <GridItem display={selectingModeVisibility} colSpan={1} w={"450px"}>
+              <Select
+                size="sm"
+                placeholder="Select template"
+                borderWidth={1}
+                borderColor="black"
+                mt={2}
+                w={"450px"}
+              >
+                <option value="task1">Task template 1</option>
+                <option value="task2">Task template 2</option>
+              </Select>
+            </GridItem>
+            <GridItem display={selectingModeVisibility} colSpan={1}>
+              <Button
+                leftIcon={<AiFillPlusCircle />}
+                size="sm"
+                ml={3}
+                mt={2}
+                pl={3}
+                pr={1}
+                borderWidth={1}
+                borderColor={"black"}
+                onClick={() => {
+                  setSelectingModeVisibility("none");
+                  setAdditionModeVisibility("inline");
+                }}
+              />
+            </GridItem>
+            <GridItem display={additionModeVisibility} colSpan={1} w={"450px"}>
+              <Input
+                size="sm"
+                value={props.taskHeader}
+                borderWidth={1}
+                borderColor="black"
+                mt={2}
+              />
+            </GridItem>
+            <GridItem display={additionModeVisibility} colSpan={1}>
+              <Button
+                leftIcon={<AiOutlineCheck />}
+                size="sm"
+                ml={3}
+                mt={2}
+                pl={3}
+                pr={1}
+                borderWidth={1}
+                borderColor={"black"}
+                onClick={() => {
+                  setSelectingModeVisibility("inline");
+                  setAdditionModeVisibility("none");
+                }}
+              />
+            </GridItem>
+          </Grid>
         </div>
         <div className="modal-footer">
           <button
@@ -58,5 +119,6 @@ const TaskTemplate = props => {
     </div>
   );
 }
+
 
 export default TaskTemplate;
